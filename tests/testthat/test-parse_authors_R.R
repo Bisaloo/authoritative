d45988f8f449baae113db80e514a4d16dc67b0db
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-02-03 12:31:53 +0100

    Make snapshot test dependent on R version

--- tests/testthat/test-parse_authors_R.R
+++ tests/testthat/test-parse_authors_R.R
@@ -5,5 +5,5 @@ test_that("parse_authors_r() snapshot", {
   cran_epidemiology_packages$`Authors@R` |>
     parse_authors_r() |>
     unique() |>
-    expect_snapshot()
+    expect_snapshot(variant = as.character(getRversion()))
 })
